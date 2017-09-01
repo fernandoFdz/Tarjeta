451a1c209886f3709cde5dffe705f0ada4c256a1
fernandoFdz <ferykudouxd@hotmail.com>
2017-09-01 03:20:22 -0500

    documentado

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,15 +1,25 @@
 public class Tarjeta{
     private float saldo;
+    
+    /*
+     * constructor de la tarjeta
+     */
     public Tarjeta(){
         saldo = 0;
     }
     
+    /*
+     * metodo en el que se deposita dinero 
+     */
     public void depositar(float dinero){
         if(saldo<0){
         saldo=saldo+dinero;
         }
     }
     
+    /*
+     * metodo para que se pueda retirar dinero 
+     */
     public float retirar(float dinero){
         if(saldo>=dinero){
             saldo=saldo-dinero;
@@ -19,7 +29,10 @@ public class Tarjeta{
         }
     }
     
-    public void consulta(float dinero){
-        
+    /*
+     * metodo para consultar el dinero que se tiene en la cuenta
+     */
+    public float consulta(float dinero){
+        return dinero;
     }
 }
